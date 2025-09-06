d97cc7043d0fa951bfcd63ecbd61414163023fce
Alef ojeda de Oliveira <nemubatubag@gmail.com>
2025-09-05 21:36:28 -0300

    feat(calculadora): adicionar método para exponenciação

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -19,4 +19,8 @@ public class Calculadora {
         }
         return a / b;
     }
+
+        public double exponenciar(double base, double expoente) {
+            return Math.pow(base, expoente);
+        }
 }
